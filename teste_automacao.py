37c69d33f08ee85f4e7658cad1ea243334162ae2
André França <39207342+androvisck@users.noreply.github.com>
2020-02-05 21:12:22 -0300

    Atualização do teste_automacao.py
    Realizada a melhoria da interface. Inseridos comentários e tempo de espera para não sobrecarrecar a plataforma que contém o formulário.  Adaptação do script à versão v2 da planilha.

--- teste_automacao.py
+++ teste_automacao.py
@@ -10,6 +10,7 @@ $ pip install requests
 """
 
 from tkinter import *
+import time
 from tkinter.filedialog import askopenfilename
 import os
 from openpyxl import load_workbook
@@ -19,20 +20,26 @@ window = Tk()
 window.title('Seja bem vindo!')
 window.geometry('400x200')
 
-label2 = Label(window, text="", font=("Arial", 10))
-label2.grid(column=0, row=0, pady=20)
+lframe = Frame(window, borderwidth=2, relief="ridge", bg="white") # Caixa com contorno e cor
+lframe.place(x=20, y=20, anchor="nw", width=360, height=40)
+label2 = Label(lframe, text="", font=("Arial", 10), bg="white", justify='center') # label que mostra o nome do arquivo
+label2.grid(column=0, row=0)
+
+label2.configure(text='')
 
 
 def click1():
     global file
     file = askopenfilename(initialdir=os.getcwd(), title="Selecione o arquivo.",
-                           filetype=(("xlsx files", "*.xlsx"), ("all files", "*.*")))
-    label2.configure(text=str(file))
+                           filetype=(("xlsx files", "*.xlsx"), ("all files", "*.*"))) # Seleção do arquivo
+    label2.configure(text=str(os.path.basename(file))) # retorno do nome do aquivo
     return file
 
 
-btn1 = Button(window, text='Escolher o arquivo', command=click1)
-btn1.grid(column=0, row=1, padx=150, pady=20)
+labelframe = Frame(window, borderwidth=1, relief="solid")
+labelframe.place(x=140, y=80, anchor="nw", width=125, height=85)
+btn1 = Button(labelframe, text='Escolher arquivo', command=click1)
+btn1.grid(column=0, row=1, padx=10, pady=10)
 
 
 def click2():
@@ -53,11 +60,20 @@ def click2():
     for x in range(1, linha + 1):
         nome = 'André José de França'
         Item = ws['A' + str(x)].value
-        CEP = ws['B' + str(x)].value
-        print(x)
+        CEP = ws['C' + str(x)].value
+        if CEP == None:
+            CEP = 'NA'
+        Endereco = ws['D' + str(x)].value
+        # verifica se o Complemento está vazio
+        if Endereco == None:
+            Endereco = 'NA'
+        Complemento = ws['E' + str(x)].value
+        # verifica se o Complemento está vazio
+        if Complemento == None:
+            Complemento = 'NA'
 
-        Endereco = ws['C' + str(x)].value
-        Complemento = ws['D' + str(x)].value
+        print(x) # verificar o funcionamento
+
         form_data = {'entry.366340186': nome, 'entry.1613519622': Item, 'entry.2045809580': CEP,
                      'entry.1334556551': Endereco, 'entry.1114882091': Complemento, 'draftResponse': [],
                      'pageHistory': 0}
@@ -67,9 +83,13 @@ def click2():
                                     "Chrome/28.0.1500.52 Safari/537.36"}
         r = requests.post(url, data=form_data, headers=user_agent)
 
+        time.sleep(0.1) # para não sobrecarregar o site que contém o formulárioa
 
-btn2 = Button(window, text='Executar', command=click2, bg='green', fg='yellow')
-btn2.grid(column=0, row=2, pady=10)
+        if x == linha:
+            label2.configure(text='Processo finalizado!') # informação de término do processo
 
+# Posição do botão executar
+btn2 = Button(labelframe, text='Executar', command=click2)
+btn2.grid(column=0, row=2)a
 
 window.mainloop()
